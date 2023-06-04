8c23218bd4b6da1fd7bcc999ab1e0d6379ac0162
SEANYB4 <bainsean@outlook.com>
2023-06-04 11:24:57 +0300

    Added dictionary corner.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-window.addEventListener('load', () => {
+let dictionary
+let keys
+
+fetch('merged.json')
+.then(response => {
+    return response.json()
+})
+.then(data => {
+    dictionary = data;
+    keys = Object.keys(dictionary);
+}).then(() => {
+    
+
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 1280;
     canvas.height = 650;
     var soundEffect = new Audio("clock.mp3");
-    roundHappening = false
+    var correctSoundEffect = new Audio("correct.mp3");
+    var roundHappening = false
+    var answer = '';
 
 
     class Game {
@@ -24,11 +38,12 @@ window.addEventListener('load', () => {
             this.roundVowels = [];
             this.roundConsonants = [];
             this.roundLetters = [];
-            this.vowelX = 500;
-            this.vowelY = 550;
-            this.consonantX = 500;
-            this.consonantY = 600;
+            this.letterX = 440;
+            this.letterY = 580;
+          
             this.points = 0;
+            this.score = 0;
+            this.runningTotal = 0;
         }
 
 
@@ -84,31 +99,32 @@ window.addEventListener('load', () => {
                 context.stroke();
             }
 
+            
 
             
             
-            // Draw the round's vowels
+            // Draw the round's letters
 
-            for (let j = 0; j < this.roundVowels.length; j++) {
+            for (let j = 0; j < this.roundLetters.length; j++) {
                 context.fillStyle = 'blue';
-                context.fillRect(this.vowelX - 15, this.vowelY - 15, 30, 30);
+                context.fillRect(this.letterX - 15, this.letterY - 15, 30, 30);
                 context.fillStyle = 'white';
-                context.fillText(this.roundVowels[j], this.vowelX, this.vowelY);
-                this.vowelX += 50;
+                context.fillText(this.roundLetters[j], this.letterX, this.letterY);
+                this.letterX += 50;
                 
             }
-            this.vowelX = 500;
+            this.letterX = 440;
 
             // Draw the round's consonants
 
-            for (let k = 0; k < this.roundConsonants.length; k++) {
-                context.fillStyle = 'blue';
-                context.fillRect(this.consonantX - 15, this.consonantY - 15, 30, 30)
-                context.fillStyle = 'white';
-                context.fillText(this.roundConsonants[k], this.consonantX, this.consonantY);
-                this.consonantX += 50;
-            }
-            this.consonantX = 500;
+            // for (let k = 0; k < this.roundConsonants.length; k++) {
+            //     context.fillStyle = 'blue';
+            //     context.fillRect(this.consonantX - 15, this.consonantY - 15, 30, 30)
+            //     context.fillStyle = 'white';
+            //     context.fillText(this.roundConsonants[k], this.consonantX, this.consonantY);
+            //     this.consonantX += 50;
+            // }
+            // this.consonantX = 500;
 
 
             // Draw reset button
@@ -131,9 +147,28 @@ window.addEventListener('load', () => {
 
             // Draw score board
 
+            context.fillStyle = 'white';
+            context.fillRect(this.width - 1200, 370, 200, 50)
             context.fillStyle = 'black';
-            context.fillText("Score:", this.width - 400, 550)
- 
+            context.fillText("Score:", this.width - 1150, 395);
+            context.fillText(this.runningTotal, this.width - 1090, 397);
+
+
+
+            // Draw dictionary corner
+
+            context.fillStyle = 'white';
+            context.fillRect(this.width - 1250, 30, 300, 200);
+            context.fillStyle = 'black';
+            context.fillText("Dictionary Corner", this.width - 1100, 50);
+            context.fillStyle = 'white';
+            context.fillRect(this.width - 1160, 170, 110, 50);
+            context.strokeStyle = 'black';
+            context.strokeRect(this.width - 1160, 170, 110, 50);
+            context.fillStyle = 'black';
+            context.fillText("Next", this.width - 1105, 195);
+
+
         }
 
         sleep(ms) {
@@ -141,23 +176,29 @@ window.addEventListener('load', () => {
         }
 
         async round(context) {
-            this.roundConsonants = []
-            this.roundVowels = []
+            this.roundConsonants = [];
+            this.roundVowels = [];
+            this.roundLetters = [];
+
+            this.score = 0;
 
             roundHappening = true
             
             let i = 0;
-            while (i < 4) {
+            while (i < 3) {
                 let index = Math.floor(Math.random()*this.vowels.length);
                 this.roundVowels.push(this.vowels[index]);
+                this.roundLetters.push(this.vowels[index]);
                 i += 1;
             }
             i = 0;
-            while (i < 7) {
+            while (i < 6) {
                 let index = Math.floor(Math.random()*this.consonants.length);
                 this.roundConsonants.push(this.consonants[index]);
+                this.roundLetters.push(this.consonants[index]);
                 i += 1;
             }
+            
 
             
             // SOUND
@@ -180,10 +221,13 @@ window.addEventListener('load', () => {
 
             }
             this.count = 44;
+            this.runningTotal += this.score;
+            this.score = 0;
             roundHappening = false
+            this.render(context);
         }
 
-      
+    
         
     }
 
@@ -196,8 +240,8 @@ window.addEventListener('load', () => {
 
     var input = document.getElementById("textInput");
     input.addEventListener("input", () => {
-        var text = input.value;
-        console.log(text);
+        answer = input.value;
+        
     })
 
 
@@ -209,12 +253,78 @@ window.addEventListener('load', () => {
         var x = e.clientX - rect.left;
         var y = e.clientY - rect.top;
 
-        if (x >= 1000 && x <= 1200 && y >= 95 && y >- 145 && !roundHappening) {
+        if (x >= 1000 && x <= 1200 && y >= 95 && y <= 145 && !roundHappening) {
             game.round(ctx);
         }
     });
+
+    // SUBMIT BUTTON EVENT LISTENER
+
+    canvas.addEventListener('click', (e) => {
+
+        var rect = canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+
+        if (x >= 1000 && y >= 450 && x <= 1200 && y <= 500 && roundHappening) {
+
+            let valid = true;
+
+            // CHECK THAT ANSWER USES CORRECT LETTERS
+
+            // for (let i =0; i<answer.length; i++) {
+            //     if (answer[i] ) {
+            //         valid = false
+            //     }
+            // }
+
+            if ((dictionary[answer.toUpperCase()] != undefined) && valid) {
+                console.log(dictionary[answer.toUpperCase()]);
+                if (game.score <= answer.length) {
+                    game.score = answer.length;
+                    correctSoundEffect.play();
+                }
+            } else {
+                let entry = document.getElementById('textInput');
+                entry.value = 'Not found'
+                console.log('not found');
+            }
+            
+            
+        }
+    });
+
+    // DICTIONARY BUTTON EVENT LISTENER
+
+    canvas.addEventListener('click', (e) => {
+
+        var rect = canvas.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+
+        if (x >= 120 && x <= 210 && y >= 170 && y <= 220 && !roundHappening) {
+            game.render(ctx)
+
+            const randomIndex = Math.floor(Math.random() * keys.length);
+            let word = keys[randomIndex];
+            let definition = dictionary[word].MEANINGS;
+            ctx.save()
+            ctx.font = "12px Arial";
+            ctx.fillText(word, 110, 110);
+            ctx.font = "10px Arial";
+            ctx.fillText(definition, 200, 130, 200);
+            ctx.restore()
+        }
+
+    });
     
     game.render(ctx);
+    })
 
+
+
+window.addEventListener('load', () => {
+
+ 
 
 });
